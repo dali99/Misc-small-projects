c39e61525f6c943da30ec3bc76d31d61bbb5e6b3
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2015-12-04 11:37:38 +0100

    Missed a paraentheses

--- CS-GO/Skins/Get Links.js
+++ CS-GO/Skins/Get Links.js
@@ -5,4 +5,4 @@ jQuery("body").append("</span></div>");
 jQuery(".sih-market-action").each(function() {
   console.log(jQuery(this).attr("href"));
   jQuery("body").append('<div style="background: white"><span>' + jQuery(this).attr("href") + ", ");
-};
+});
