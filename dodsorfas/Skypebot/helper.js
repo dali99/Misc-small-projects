75e5069dd1b2ad60b407b1f8fb556ae658e25927
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2016-08-21 22:10:46 +0200

    Srs, another variant.
    Hopefully that covers all of them

--- dodsorfas/Skypebot/helper.js
+++ dodsorfas/Skypebot/helper.js
@@ -5,5 +5,5 @@ exports.send = function(session, texta) {
 }
 
 exports.regex = function(command) {
-	return RegExp ("^(<at id=\"\\S+\">(Dodsorbot|@28:b8381d78-ab09-496c-b382-860a98fa1dc3)<\\/at>\\s)?(" + command + ")", "i");
+	return RegExp ("^(<at id=\"\\S+\">(@?Dodsorbot|@28:b8381d78-ab09-496c-b382-860a98fa1dc3)<\\/at>\\s)?(" + command + ")", "i");
 }
