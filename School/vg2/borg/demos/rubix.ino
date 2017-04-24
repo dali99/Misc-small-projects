80823df95e8e42528d084d1fa6143b0afe983e48
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2017-04-24 07:06:26 +0200

    Update rubix.ino

--- School/vg2/borg/demos/rubix.ino
+++ School/vg2/borg/demos/rubix.ino
@@ -16,57 +16,63 @@ CRGB leds[NUM_LEDS];
 CRGB solution[NUM_LEDS];
 
 void setup() {
-	FastLED.addLeds<PL9823, DATA_PIN>(leds, NUM_LEDS);
-	initMap();
-	randomSeed(analogRead(0));
-	FastLED.clear();
-	FastLED.show();
+  FastLED.addLeds<PL9823, DATA_PIN>(leds, NUM_LEDS);
+  initMap();
+  randomSeed(analogRead(0));
+  FastLED.clear();
+  FastLED.show();
 
-	// Make the solution memory structure
-	setColor({0, 255, 255}, BLUE, solution);
-	setColor({1, 255, 255}, RED, solution);
-	setColor({2, 255, 255}, GREEN, solution);
-	setColor({3, 255, 255}, ORANGE, solution);
-	setColor({4, 255, 255}, WHITE, solution);
-	setColor({5, 255, 255}, YELLOW, solution);
-	
-	setColor({0, 1, 1}, BLUE, leds);
-	setColor({1, 1, 1}, RED, leds);
-	setColor({2, 1, 1}, GREEN, leds);
-	setColor({3, 1, 1}, ORANGE, leds);
-	setColor({4, 1, 1}, WHITE, leds);
-	setColor({5, 1, 1}, YELLOW, leds);
+  // Make the solution memory structure
+  setColor({0, 255, 255}, BLUE, solution);
+  setColor({1, 255, 255}, RED, solution);
+  setColor({2, 255, 255}, GREEN, solution);
+  setColor({3, 255, 255}, ORANGE, solution);
+  setColor({4, 255, 255}, WHITE, solution);
+  setColor({5, 255, 255}, YELLOW, solution);
 
-	for (byte i = 0; i < 6; i++) {    
-		for (byte j = 0; j < 8; j++) {
-			byte selection;
-			do {
-				selection = random(0, NUM_LEDS);
-			}
-			while (leds[selection] != (CRGB) 0x000000);
+  setColor({0, 1, 1}, BLUE, leds);
+  setColor({1, 1, 1}, RED, leds);
+  setColor({2, 1, 1}, GREEN, leds);
+  setColor({3, 1, 1}, ORANGE, leds);
+  setColor({4, 1, 1}, WHITE, leds);
+  setColor({5, 1, 1}, YELLOW, leds);
+
+  for (byte i = 0; i < 6; i++) {
+    FastLED.show();
+    delay(500);
+    for (byte j = 0; j < 8; j++) {
+      byte selection;
+      do {
+        selection = random(0, NUM_LEDS);
+      }
+      while (leds[selection] != (CRGB) 0x000000);
       
-			switch (i) {
-			case 0:
-				leds[selection] = BLUE;
-				break;
-			case 1:
-				leds[selection] = RED;
-				break;
-			case 2:
-				leds[selection] = GREEN;
-				break;
-			case 3:
-				leds[selection] = ORANGE;
-				break;
-			case 4:
-				leds[selection] = WHITE;
-				break;
-			case 5:
-				leds[selection] = YELLOW;
-				break;
-			}
-		}
-	}
-	FastLED.show();
+      switch (i) {
+      case 0:
+         leds[selection] = BLUE;
+         break;
+      case 1:
+        leds[selection] = RED;
+        break;
+      case 2:
+        leds[selection] = GREEN;
+        break;
+      case 3:
+        leds[selection] = ORANGE;
+        break;
+      case 4:
+        leds[selection] = WHITE;
+        break;
+      case 5:
+        leds[selection] = YELLOW;
+        break;
+      }
+    }
+  }
+
+  FastLED.show();
 }
-void loop() {}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
